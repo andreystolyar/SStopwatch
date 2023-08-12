b78f6a8af8e8f51d0f5eecb611df966dfc15e1e7
andreystolyar <andreystolyar21@gmail.com>
2023-08-12 17:59:10 +0300

    Three buttons and a pin button

--- SStopwatch/ViewModels/StopwatchViewModel.cs
+++ SStopwatch/ViewModels/StopwatchViewModel.cs
@@ -17,24 +17,16 @@ internal class StopwatchViewModel : INotifyPropertyChanged
     public StopwatchViewModel(IStopwatchModel model)
     {
         this.model = model;
-        model.TimeChanged += Model_TimeChanged;
+
+        model.TimeChanged +=
+            delegate { PropertyChanged!(this, new PropertyChangedEventArgs(nameof(Time))); };
 
         Start = new CustomCommand(_ => model.StartAsync(), _ => !model.IsRunning);
         Pause = new CustomCommand(_ => model.Pause(), _ => model.IsRunning);
         Stop = new CustomCommand(_ => model.Stop());
     }
 
-    private void Model_TimeChanged(object? sender, EventArgs e)
-    {
-        Time = model.Time;
-    }
+    public TimeSpan Time => model.Time;
 
-    public TimeSpan Time
-    {
-        get => model.Time;
-        set => PropertyChanged?.Invoke
-            (this, new PropertyChangedEventArgs(nameof(Time)));
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged = delegate { };
 }
